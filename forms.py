37b701462843cdfe7837660ee992f4b5d25ceb67
Hedgehawk11 <84346970+Hedgehawk11@users.noreply.github.com>
2024-01-31 17:11:21 +0000

    IT WORKS

--- forms.py
+++ forms.py
@@ -1,1 +1,20 @@
+import pymongo
+from pymongo import MongoClient
+import sys
 
+
+def setup(uri):
+  global client
+  client = MongoClient(uri)
+  try:
+    client.admin.command('ping')
+    print("Connected to server")
+  except Exception as e:
+    print('ruh roh raggy')
+
+def register(name,answer):
+  #edit the names of db and collection to your mongo db and collection names
+  db = client.put_yours_here
+  collection = db.put_yours_here
+  collection.insert_one({"name":name,"answer":answer})
+  print("Registered")
---
+++ main.py
@@ -0,0 +1,4 @@
+import forms
+
+forms.setup("uri")
+forms.register("name","answer")
